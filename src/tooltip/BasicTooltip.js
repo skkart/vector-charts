16b0dc32f6fe6b8319def00e54cd62aac1500ddb
skkart <skkarthik10@gmail.com>
2019-10-20 02:27:29 +0530

    Removed jquery

--- src/tooltip/BasicTooltip.js
+++ src/tooltip/BasicTooltip.js
@@ -1,5 +1,5 @@
 import ChartComponent from '@/charts/ChartComponent'
-import {isFunction} from '@/utils'
+import {elementOffset, isFunction} from '@/utils'
 
 export default class BasicTooltip extends ChartComponent {
   constructor (opts) {
@@ -17,40 +17,36 @@ export default class BasicTooltip extends ChartComponent {
   }
 
   draw () {
-    this.$toolTipDiv = $('<div></div>').addClass('vc-tooltip-display ' + this.opts.className)
-    this.opts.chart.$container
-      .append(this.$toolTipDiv)
+    this.toolTipDiv = this.opts.chart.container
+      .append('div')
+      .attr('class', 'vc-tooltip-display ' + this.opts.className)
   }
 
   hover (x, y, displayData, excludeToolTipDiv) {
+    this.showHide(true)
     // This will be called by series -- bar / pie
-    var rootPos = this.opts.chart.$container.offset()
-    var xpos = Math.round(x - rootPos.left)
-    var ypos = Math.round(y - rootPos.top)
+    const box = elementOffset(this.toolTipDiv)
+    const rootPos = elementOffset(this.opts.chart.container)
+    let xpos = Math.round(x - rootPos.left)
+    let ypos = Math.round(y - rootPos.top)
 
     if (!excludeToolTipDiv) {
       // Consider toolTip offset for position
-      xpos = Math.round(xpos - (this.$toolTipDiv.width() / 2))
-      ypos = Math.round(ypos - (this.$toolTipDiv.height() / 2))
+      xpos = Math.round(xpos - (box.width / 2))
+      ypos = Math.round(ypos - (box.height / 2))
     }
 
-    this.$toolTipDiv
-      .css({
-        left: xpos + 'px',
-        top: ypos + 'px',
-        display: 'inline-block'
-      })
+
+    this.toolTipDiv
+      .style('left', xpos + 'px')
+      .style('top', ypos + 'px')
+      .style('display', 'inline-block')
       .html(this.opts.format.call(this.opts.chart, displayData.data)) // Used format.call(), so that external fun can have access of chart Instance
   }
 
   showHide (showFlag) {
     showFlag = !!showFlag
-
-    if (showFlag) {
-      this.$toolTipDiv && this.$toolTipDiv.show()
-    } else {
-      this.$toolTipDiv && this.$toolTipDiv.hide()
-    }
+    this.toolTipDiv && this.toolTipDiv.classed('vc-hidden', !showFlag)
   }
 
   remove () {
