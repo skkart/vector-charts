3c0a743d6b65076aebe9b3e1b71a16bbe30bb3e9
skkart <skkarthik10@gmail.com>
2019-11-07 18:28:19 +0530

    Build files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,10 @@
+import config from './webpack.config'
 const path = require('path')
-// const webpack = require('webpack')
-const SRC_DIR = path.resolve(__dirname, './src/')
-var config = {
+
+module.exports = {
+  ...config,
   mode: 'development',
   devtool: 'inline-source-map',
-  context: SRC_DIR, // `__dirname` is root of project and `/src` is source
-  entry: {
-    'vector-charts': path.resolve(SRC_DIR, 'index.js'),
-  },
   output: {
     path: path.resolve(__dirname, 'dist'), // `/dist` is the destination
     filename: '[name].js',
@@ -15,25 +12,5 @@ var config = {
     libraryTarget: 'umd',
     umdNamedDefine: true,
     libraryExport: 'default'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // rule for .js files
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'] // apply this loader for js files
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.join(__dirname, 'src'),
-    }
-  },
-  stats: {
-    colors: true
   }
 }
-
-module.exports = config
